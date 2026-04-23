9d7e051f81547aa962ea4486fc7b12bed0c42603
mystify0007 <karkibeni12@gmail.com>
2026-04-23 20:48:33 +0545

    first set up

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+def get_requirements(file_path:str) -> List[str]:
+    """
+    this function will return the list of requirements
+    """
+
+    requirments = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace ('\n',"") for req in requirements]
+    return requirements
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Beni',
+    author_email = 'karkibeni12@gmail.com',
+    packages =  find_packages(),
+    install_requires = get_requirements('requirement.txt')
+)
